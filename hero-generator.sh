8b804fb1853916a43f9cfe50d31bbcb89636d711
elhenro <github@henry-schober.com>
2017-10-29 20:55:34 +0100

    first working version

---
+++ hero-generator.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# automatic hero image producer
+# usage: hnr@hnr-b:~/web/CI-generator$ sh hero-generation-script.sh blank_hero.jpg 'Test Titel' script-res
+# --> $ sh hero-generation-script.sh hero-bg.jpg 'CTA TEXT' final-name.jpg
+# args
+INPUT="$1"
+TITLE="$2"
+FILENAME="$3"
+# put title text in html
+sed -i "s/TEXT/${TITLE}/" button.html
+# render CTA from button.html
+phantomjs /usr/local/share/phantomjs-1.9.8-linux-x86_64/examples/rasterize.js /home/hnr/web/ci-generator/button.html exp.png
+# merge images southeast - put CTA on image
+composite -gravity southeast exp.png $INPUT $FILENAME
+# End
+# replace CTA text back
+sed -i "s/${TITLE}/TEXT/" button.html
+echo "hero-generator.sh successful."
+export LANG=C.UTF-8
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # $ python3 main.py 'Der Titel' https://url.com
 import sys
-import os
-from lxml import html
-import requests
-import datetime
-from PIL import Image, ImageOps
+import os  # for os line breaks and bash
+from lxml import html  # for reading dom
+import requests  # for web action
+from shutil import copyfile  # for file operations
+import datetime  # for generating timestamp
+from PIL import Image, ImageOps  # for image resize and crop
+import subprocess  # for running shell scripts
 
 TimeNow = (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 item_title = (sys.argv[1])
@@ -12,16 +14,22 @@ item_url = (sys.argv[2])
 kleinb = item_title.lower()
 
 kleinbdash = kleinb.replace(' ', '-').lower()
-jpegFileName = kleinbdash + '.jpg'
+jpegFileName = kleinbdash + '.jpg'  # TODO: Umlaute
+heroName = 'hero_'+jpegFileName
 print (jpegFileName)
 
 # log function
 def logger ( entry ) :
+    # print ( entry )  # debug
     f = open('log/log.txt', 'r+')
     f.read()
     f.write(entry + "\n")
     f.close()
     return
+
+#log input
+input_entry = TimeNow + ' got input: ' + item_title + '  Url: ' + item_url
+logger(input_entry)
 
 # log Title and filename
 name_entry = TimeNow + ' generating filename: '+ jpegFileName + ' from title: ' + item_title
@@ -46,11 +54,13 @@ print ('Image Download Link: ' + imagelink)
 
 # download image
 print ('Downloading...')
-logfile = open('workbench/'+jpegFileName, 'wb')
-logfile.write(requests.get(imagelink).content)
-logfile.close()
-print('Download successful.')
-# TODO: Log download
+f = open('workbench/' + jpegFileName, 'wb')
+f.write(requests.get(imagelink).content)
+f.close()
+logger(TimeNow + ' Download of ' + imagelink + ' Successful.')
+# copy image for hero
+# call(["bash", "cp workbench/"+jpegFileName+' workbench_hero/'+heroName])
+copyfile('workbench/'+jpegFileName, 'workbench_hero/'+heroName)
 
 # crop image
 print ('Generating Images...')
@@ -70,6 +80,26 @@ thumb = ImageOps.fit(img, (100, 100), Image.ANTIALIAS)
 # img = ImageOps.fit((100, 100), Image.ANTIALIAS)
 thumb.save('data/square/'+jpegFileName)  # save down large image
 print ('Images generated successful.')
-# TODO: Log resized and cropped
+logger(TimeNow + ' successfully resized and cropped large - square')
 
+# HERO SECTION
+logger('Starting hero-image production...')
+# crop hero
+img = Image.open('workbench_hero/' + heroName)  # read hero image
+# img = img.resize((960, 250), Image.ANTIALIAS)  # resize
+hero = ImageOps.fit(img, (960, 250), Image.ANTIALIAS)
+hero.save('workbench_hero/' + heroName)  # save down hero image to workbench
+logger(TimeNow + ' Starting shell script for CTA and Hero-image..')
+proj_path = '/home/hnr/web/ci-generator/'
+hero_path = proj_path + 'workbench_hero/' + heroName
+# generate CTA from button.html and -.css and merge with hero
+os.system('sh hero-generator.sh workbench_hero/' + heroName + ' "' + item_title + '" workbench_hero/' + heroName)
 
+copyfile('workbench_hero/'+heroName, 'hero/large/'+heroName)
+
+img = Image.open('workbench_hero/' + heroName)  # read hero image again (now with button)
+# generate medium and small version
+img = img.resize((500, 130), Image.ANTIALIAS) # resize
+img.save('hero/medium/'+heroName)
+img = img.resize((300, 78), Image.ANTIALIAS) # resize
+img.save('hero/small/'+heroName)
