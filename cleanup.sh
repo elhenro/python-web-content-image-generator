b2b066344dcb1cb50e980df812475770c853a2a8
elhenro <github@henry-schober.com>
2017-11-18 12:48:13 +0100

    cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf data/large/*;
+rm -rf data/medium/*;
+rm -rf data/small/*;
+rm -rf data/square/*;
+rm -rf workbench/*;
+rm -rf workbench_hero/*;
+rm -rf hero/large/*;
+rm -rf hero/medium/*;
+rm -rf hero/small/*;
