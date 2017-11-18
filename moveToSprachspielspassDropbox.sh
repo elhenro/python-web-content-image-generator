d33c152e09e402db3a3f637537dcf192747fb5e2
elhenro <github@henry-schober.com>
2017-11-18 14:14:04 +0100

    move script

---
+++ moveToSprachspielspassDropbox.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PATH_CG=~/web/ci-generator
+#PATH_CONTENT="~/Dropbox/Sprachspielspass.de/#contentprocess/New/01\ Optimised\ Pictures"
+#PATH_HERO="~/Dropbox/Sprachspielspass.de/#contentprocess/New/03\ Hero\ Images"
+#PATH_CONTENT="~/Dropbox/Sprachspielspass.de/#contentprocess/New/02\ Content\ Pictures"
+
+cp -r data/* ~/Dropbox/Sprachspielspass.de/#contentprocess/New/01\ Optimised\ Pictures/
+cp -r hero/* ~/Dropbox/Sprachspielspass.de/#contentprocess/New/03\ Hero\ Images/
+cp -r workbench/* ~/Dropbox/Sprachspielspass.de/#contentprocess/New/02\ Content\ Pictures/
+
+#mv $PATH_CG/data/large/* $PATH_CONTENT/large
+#mv "${PATH_CG}/data/medium/*" "${PATH_CONTENT}/medium"
+#mv "${PATH_CG}/data/small/*" "${PATH_CONTENT}/small"
+#mv "${PATH_CG}/data/square/*" "${PATH_CONTENT}/square"
+#mv "${PATH_CG}/hero/large/*" "${PATH_HERO}/large"
+#mv "${PATH_CG}/hero/medium/*" "${PATH_HERO}/medium"
+#mv "${PATH_CG}/hero/small/*" "${PATH_HERO}/small"
+#mv "${PATH_CG}/workbench/*" "${PATH_CONTENT}"
+
+
+
+
+
+
+
+
